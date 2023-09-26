fb4e420301a056c9c253b938bb772fe65e8a10e4
op-coder <100160944+nagu-op@users.noreply.github.com>
2023-09-26 14:23:16 +0530

    Add files via upload

---
+++ NameInput.java
@@ -0,0 +1,12 @@
+package firstJavaLab;
+import java.util.Scanner;
+public class NameInput {
+	public static void main(String[]args) {
+		System.out.println(" Enter the number: ");
+		int num;
+		Scanner sc = new Scanner(System.in);
+		num = sc.nextInt();
+		System.out.println("The number is "+num);
+		
+		}
+}
