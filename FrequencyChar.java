77545c023909df20d390fce42ef18d23e9c9bb23
op-coder <100160944+nagu-op@users.noreply.github.com>
2023-10-03 15:59:31 +0530

    Add files via upload

---
+++ FrequencyChar.java
@@ -0,0 +1,27 @@
+package firstJavaLab;
+import java.util.Scanner;
+public class FrequencyChar {
+	public static void main(String [] args) {
+		String userInput;
+		System.out.println("input the string: ");
+		Scanner sc = new Scanner(System.in);
+		userInput = sc.next();
+		char findChar;
+		System.out.println("input the char");
+		findChar = sc.next().charAt(0);
+		char []newArray = userInput.toCharArray();
+		int count = 0;
+		count = countChecker(newArray,userInput,findChar);
+		System.out.println("The count of the " + findChar + " is "+count);
+	}
+	static int countChecker(char [] newArray, String userInput,char findChar) {
+		int sentenceLength = userInput.length(),count = 0;
+		for (int i = 0; i<sentenceLength;i++) {
+			if (findChar == newArray[i] ) {
+				count = count +1;
+			}
+		}
+		return count;
+	}
+
+}
